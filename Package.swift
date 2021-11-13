ae35c9b3667de29eea03d5304dde7b07046dd606
LUO Sheng <luosheng1986@gmail.com>
2021-11-13 22:52:41 +0800

    Add ShellOut as a dependency

--- Package.swift
+++ Package.swift
@@ -10,10 +10,13 @@ let package = Package(
         .executable(name: "arm64-to-sim", targets: ["arm64-to-sim"])
     ],
     dependencies: [
+        .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.0.0")
     ],
     targets: [
         .target(
             name: "arm64-to-sim",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ShellOut", package: "ShellOut")
+            ]),
     ]
 )
