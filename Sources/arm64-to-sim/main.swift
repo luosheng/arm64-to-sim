7609241d87b9b6b3c37cc2c4dfefa361e3077c6f
LUO Sheng <luosheng1986@gmail.com>
2021-11-14 01:06:01 +0800

    Read minos and sdk from environment

--- Sources/arm64-to-sim/main.swift
+++ Sources/arm64-to-sim/main.swift
@@ -217,6 +217,14 @@ guard CommandLine.arguments.count > 1 else {
 let binaryPath = CommandLine.arguments[1]
 var minos: UInt32 = 13
 var sdk: UInt32 = 13
+if let minosStr = ProcessInfo.processInfo.environment["ARM64_TO_SIM_MINOS"],
+    let number = UInt32(minosStr) {
+    minos = number
+}
+if let sdkStr = ProcessInfo.processInfo.environment["ARM64_TO_SIM_SDK"],
+   let number = UInt32(sdkStr) {
+    sdk = number
+}
 if CommandLine.arguments.count > 3 {
     if let number = UInt32(CommandLine.arguments[2]) {
         minos = number
