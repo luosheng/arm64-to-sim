11e714dda4907544b047a01a3d069b418446fe8a
LUO Sheng <luosheng1986@gmail.com>
2021-12-03 14:08:56 +0800

    Split files

---
+++ Sources/arm64-to-sim/Extensions.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by Luo Sheng on 2021/12/3.
+//
+
+import Foundation
+
+// support checking for Mach-O `cmd` and `cmdsize` properties
+extension Data {
+    var loadCommand: UInt32 {
+        let lc: load_command = withUnsafeBytes { $0.load(as: load_command.self) }
+        return lc.cmd
+    }
+    
+    var commandSize: Int {
+        let lc: load_command = withUnsafeBytes { $0.load(as: load_command.self) }
+        return Int(lc.cmdsize)
+    }
+    
+    func asStruct<T>(fromByteOffset offset: Int = 0) -> T {
+        return withUnsafeBytes { $0.load(fromByteOffset: offset, as: T.self) }
+    }
+}
+
+extension Array where Element == Data {
+    func merge() -> Data {
+        return reduce(into: Data()) { $0.append($1) }
+    }
+}
+
+// support peeking at Data contents
+extension FileHandle {
+    func peek(upToCount count: Int) throws -> Data? {
+        // persist the current offset, since `upToCount` doesn't guarantee all bytes will be read
+        let originalOffset = offsetInFile
+        let data = try read(upToCount: count)
+        try seek(toOffset: originalOffset)
+        return data
+    }
+}
