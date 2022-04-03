dd412c11373d922ebdb99c25f03faabedd9b512e
LUO Sheng <luosheng1986@gmail.com>
2022-04-03 11:14:06 +0800

    Update transmogrifier with upstream to handle S_ZEROFILL

--- Sources/arm64-to-sim/Transmogrifier.swift
+++ Sources/arm64-to-sim/Transmogrifier.swift
@@ -9,7 +9,7 @@ import Foundation
 import MachO
 
 struct Transmogrifier {
-    private static func readBinary(atPath path: String) -> (Data, [Data], Data) {
+    private static func readBinary(atPath path: String, isDynamic: Bool = false) -> (Data, [Data], Data) {
         guard let handle = FileHandle(forReadingAtPath: path) else {
             fatalError("Cannot open a handle for the file at \(path). Aborting.")
         }
@@ -25,6 +25,11 @@ struct Transmogrifier {
         let loadCommandsData: [Data] = (0..<header.ncmds).map { _ in
             let loadCommandPeekData = try! handle.peek(upToCount: MemoryLayout<load_command>.stride)
             return try! handle.read(upToCount: Int(loadCommandPeekData!.commandSize))!
+        }
+        
+        if isDynamic {
+            let bytesToDiscard = abs(MemoryLayout<build_version_command>.stride - MemoryLayout<version_min_command>.stride)
+            _ = handle.readData(ofLength: bytesToDiscard)
         }
         
         let programData = try! handle.readToEnd()!
@@ -49,6 +54,13 @@ struct Transmogrifier {
         segment.vmsize += UInt64(offset)
         
         let offsetSections = sections.map { section -> section_64 in
+            let sectionType = section.flags & UInt32(SECTION_TYPE)
+            switch Int32(sectionType) {
+            case S_ZEROFILL, S_GB_ZEROFILL, S_THREAD_LOCAL_ZEROFILL:
+                return section
+            case _: break
+            }
+            
             var section = section
             section.offset += UInt32(offset)
             section.reloff += section.reloff > 0 ? UInt32(offset) : 0
@@ -89,31 +101,94 @@ struct Transmogrifier {
         return Data(bytes: &command, count: data.commandSize)
     }
     
-    static func processBinary(atPath path: String, minos: UInt32 = 13, sdk: UInt32 = 13) {
-        let (headerData, loadCommandsData, programData) = readBinary(atPath: path)
+    private static func computeLoadCommandsEditor(_ loadCommandsData: [Data], isDynamic: Bool) -> ((Data, UInt32, UInt32) -> Data) {
         
+        if isDynamic {
+            return updateDylibFile
+        }
+        
+        var contains_LC_VERSION_MIN_IPHONEOS = false
+        var contains_LC_BUILD_VERSION = false
+        for lc in loadCommandsData {
+            let loadCommand = UInt32(lc.loadCommand)
+            if loadCommand == LC_VERSION_MIN_IPHONEOS {
+                contains_LC_VERSION_MIN_IPHONEOS = true
+            } else if loadCommand == LC_BUILD_VERSION {
+                contains_LC_BUILD_VERSION = true
+            }
+        }
+        
+        if contains_LC_VERSION_MIN_IPHONEOS == contains_LC_BUILD_VERSION {
+            if contains_LC_BUILD_VERSION == true {
+                fatalError("Bad Mach-O Object file: Both LC_VERSION_MIN_IPHONEOS and LC_BUILD_VERSION are present.\nEither one of them should be present")
+            } else {
+                fatalError("Bad Mach-O Object file: does not contain LC_VERSION_MIN_IPHONEOS or LC_BUILD_VERSION.\nEither one of them should be present")
+            }
+        }
+        
+        if contains_LC_VERSION_MIN_IPHONEOS {
+            // `offset` is kind of a magic number here, since we know that's the only meaningful change to binary size
+            // having a dynamic `offset` requires two passes over the load commands and is left as an exercise to the reader
+            return updatePreiOS12ObjectFile
+        } else {
+            return updatePostiOS12ObjectFile
+        }
+    }
+    
+    
+    static func updatePostiOS12ObjectFile(lc: Data, minos: UInt32, sdk: UInt32) -> Data {
+        let cmd = Int32(bitPattern: lc.loadCommand)
+        switch cmd {
+        case LC_BUILD_VERSION:
+            return updateVersionMin(lc, 0, minos: minos, sdk: sdk)
+        default:
+            return lc
+        }
+    }
+    
+    static func updatePreiOS12ObjectFile(lc: Data, minos: UInt32, sdk: UInt32) -> Data {
         // `offset` is kind of a magic number here, since we know that's the only meaningful change to binary size
         // having a dynamic `offset` requires two passes over the load commands and is left as an exercise to the reader
         let offset = UInt32(abs(MemoryLayout<build_version_command>.stride - MemoryLayout<version_min_command>.stride))
+        let cmd = Int32(bitPattern: lc.loadCommand)
+        switch  cmd {
+        case LC_SEGMENT_64:
+            return updateSegment64(lc, offset)
+        case LC_VERSION_MIN_IPHONEOS:
+            return updateVersionMin(lc, offset, minos: minos, sdk: sdk)
+        case LC_DATA_IN_CODE, LC_LINKER_OPTIMIZATION_HINT:
+            return updateDataInCode(lc, offset)
+        case LC_SYMTAB:
+            return updateSymTab(lc, offset)
+        case LC_BUILD_VERSION:
+            return updateVersionMin(lc, offset, minos: minos, sdk: sdk)
+        default:
+            return lc
+        }
+    }
+    
+    static func updateDylibFile(lc: Data, minos: UInt32, sdk: UInt32) -> Data {
+        // `offset` is kind of a magic number here, since we know that's the only meaningful change to binary size
+        // having a dynamic `offset` requires two passes over the load commands and is left as an exercise to the reader
+        let offset = UInt32(abs(MemoryLayout<build_version_command>.stride - MemoryLayout<version_min_command>.stride))
+        let cmd = Int32(bitPattern: lc.loadCommand)
+        guard cmd != LC_BUILD_VERSION else {
+            fatalError("This arm64 binary already contains an LC_BUILD_VERSION load command!")
+        }
+        if cmd == LC_VERSION_MIN_IPHONEOS {
+            return updateVersionMin(lc, offset, minos: minos, sdk: sdk)
+        }
+        return lc
+    }
+    
+    
+    public static func processBinary(atPath path: String, minos: UInt32 = 13, sdk: UInt32 = 13, isDynamic: Bool = false) {
+        let (headerData, loadCommandsData, programData) = readBinary(atPath: path, isDynamic: isDynamic)
+        
+        let editor = computeLoadCommandsEditor(loadCommandsData, isDynamic: isDynamic)
         
         let editedCommandsData = loadCommandsData
-            .map { (lc) -> Data in
-                switch Int32(lc.loadCommand) {
-                case LC_SEGMENT_64:
-                    return updateSegment64(lc, offset)
-                case LC_VERSION_MIN_IPHONEOS:
-                    return updateVersionMin(lc, offset, minos: minos, sdk: sdk)
-                case LC_DATA_IN_CODE, LC_LINKER_OPTIMIZATION_HINT:
-                    return updateDataInCode(lc, offset)
-                case LC_SYMTAB:
-                    return updateSymTab(lc, offset)
-                case LC_BUILD_VERSION:
-                    print("This arm64 binary has already been patched.")
-                    exit(0)
-                default:
-                    return lc
-                }
-            }
+            .map { return editor($0, minos, sdk) }
             .merge()
         
         var header: mach_header_64 = headerData.asStruct()
